c23c7866b8cdfa093bea5958739b93723f4a86c9
dania <danialkabduluahit201@gmail.com>
2025-12-16 20:55:40 +0500

    Added methods to the EventManager for comparing and also added fields ID for event and participant classes

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,6 @@ public class Main{
         Event event2=new Event(2,"Team training","Main Sports centre","20th January at 19:00","Only elder group!");
         Participant participant1=new Participant(1,"Oralbai Zhanat",16,"ozhanat2009@gmail.com");
         Participant participant2=new Participant(2,"Kabduluakhit Danial",17,"danialkabuluahit201@gmail.com");
-        EventManager.CompareParticipants(participant1,participant2);
         EventManager eventmanager1=new EventManager(event1,participant1,"vip");
         EventManager eventmanager2=new EventManager(event2,participant2,"standard");
         event1.displayInfo();
