b09051e09869af2f992071f4e9b42e754e1aa100
dania <danialkabduluahit201@gmail.com>
2026-01-21 22:26:13 +0500

    Final part.

---
+++ src/com/eventmanagement/DatabaseOperations.java
@@ -0,0 +1,259 @@
+package com.eventmanagement;
+
+import java.sql.*;
+import java.util.Scanner;
+
+public class DatabaseOperations {
+    private static final String DB_URL = "jdbc:postgresql://localhost:5432/Assignment(java)";
+    private static final String DB_USER = "postgres";
+    private static final String DB_PASSWORD = "200888";
+    public static void addFemaleParticipantToDB(Scanner sc) {
+        System.out.print("First name: ");
+        String firstName = sc.nextLine();
+        System.out.print("Last name: ");
+        String lastName = sc.nextLine();
+        System.out.print("Age: ");
+        int age = sc.nextInt();
+        sc.nextLine();
+        System.out.print("Email: ");
+        String email = sc.nextLine();
+        System.out.print("T-shirt size: ");
+        String tShirtSize = sc.nextLine();
+        FemaleParticipant f_p=new FemaleParticipant(firstName+lastName,age,email,tShirtSize);
+        DataPool.addParticipant(f_p);
+
+        String sql = "INSERT INTO female_participants (first_name, last_name, age,t_shirt_size,email) VALUES (?, ?, ?, ?, ?) ";
+
+        try (Connection con = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+             PreparedStatement pstmt = con.prepareStatement(sql)) {
+
+            pstmt.setString(1, firstName);
+            pstmt.setString(2, lastName);
+            pstmt.setInt(3, age);
+            pstmt.setString(4, tShirtSize);
+            pstmt.setString(5, email);
+
+            int rowsAffected = pstmt.executeUpdate();
+            if (rowsAffected > 0) {
+                System.out.println("Female participant added to database!");
+            }
+        } catch (SQLException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+    public static void addMaleParticipantToDB(Scanner sc) {
+        System.out.println("First name: ");
+        String first_name = sc.nextLine();
+        System.out.println("Last name: ");
+        String last_name = sc.nextLine();
+        System.out.println("Age: ");
+        int age = sc.nextInt();
+        sc.nextLine();
+        System.out.println("T-shirt size: ");
+        String T_shirt_size = sc.nextLine();
+        System.out.println("Email: ");
+        String email=sc.nextLine();
+
+        MaleParticipant m_p=new MaleParticipant(first_name+last_name,age,email,T_shirt_size);
+        DataPool.addParticipant(m_p);
+        String sql = "INSERT INTO male_participants (first_name,last_name,age,t_shirt_size,email) VALUES (?,?,?,?,?)";
+
+        try (Connection con = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+             PreparedStatement pstmt = con.prepareStatement(sql)) {
+            pstmt.setString(1, first_name);
+            pstmt.setString(2, last_name);
+            pstmt.setInt(3, age);
+            pstmt.setString(4, T_shirt_size);
+            pstmt.setString(5, email);
+            int rowsAffected = pstmt.executeUpdate();
+            if (rowsAffected > 0) System.out.println("Female participant added to database!");
+        } catch (SQLException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+    public static void viewAllFemaleParticipants(){
+        String sql="SELECT * FROM female_participants";
+        try(Connection con = DriverManager.getConnection(DB_URL,DB_USER,DB_PASSWORD);
+            Statement stmt = con.createStatement();
+            ResultSet rs=stmt.executeQuery(sql)) {
+            System.out.println("Female participants");
+            while(rs.next()){
+                System.out.println(
+                        "ID: " + rs.getInt("female_id") +
+                                ", Name: " + rs.getString("first_name") + " " + rs.getString("last_name") +
+                                ", Age: " + rs.getInt("age") +
+                                ", T-Shirt size: " + rs.getString("t_shirt_size") +
+                                ", Email: " + rs.getString("email")
+                );
+            }
+        }catch(SQLException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+    public static void viewAllMaleParticipants(){
+        String sql="SELECT * FROM male_participants ORDER BY age";
+        try(Connection con=DriverManager.getConnection(DB_URL,DB_USER,DB_PASSWORD);
+            Statement stmt=con.createStatement();
+            ResultSet rs=stmt.executeQuery(sql)) {
+            System.out.println("Male participants");
+            while(rs.next()){
+                System.out.println(
+                        "ID: " + rs.getInt("male_id") +
+                                ", Name: " + rs.getString("first_name") + " " + rs.getString("last_name") +
+                                ", Age: " + rs.getInt("age") +
+                                ", T-Shirt size: " + rs.getString("t_shirt_size") +
+                                ", Email: " + rs.getString("email")
+                );
+            }
+        }catch(SQLException e) {
+            System.out.println("Error: "+e.getMessage());
+        }
+    }
+    public static void updateParticipant(Scanner sc) {
+        System.out.println("Gender (M/F): ");
+        String gender = sc.nextLine();
+        System.out.println("Participant ID");
+        int id = sc.nextInt();
+        sc.nextLine();
+        System.out.println("\nWhat to update?");
+        System.out.println("1. First Name");
+        System.out.println("2. Last Name");
+        System.out.println("3. Age");
+        System.out.println("4. Email");
+        System.out.println("5. T-Shirt Size");
+        System.out.println("How many columns to update?");
+        System.out.print("Choose: ");
+        int updateChoice = sc.nextInt();
+        sc.nextLine();
+
+        String tableName = gender.equalsIgnoreCase("F") ? "female_participants" : "male_participants";
+        String idColumn = gender.equalsIgnoreCase("F") ? "female_id" : "male_id";
+        String columnToUpdate = "";
+
+        switch (updateChoice) {
+            case 1:
+                columnToUpdate = "first_name";
+                break;
+            case 2:
+                columnToUpdate = "last_name";
+                break;
+            case 3:
+                columnToUpdate = "age";
+                break;
+            case 4:
+                columnToUpdate = "email";
+                break;
+            case 5:
+                columnToUpdate = "t_shirt_size";
+                break;
+            default:
+                System.out.println("Invalid choice!");
+                return;
+        }
+
+        System.out.print("Enter new value: ");
+        String newValue = sc.nextLine();
+
+        String sql = "UPDATE " + tableName + " SET " + columnToUpdate + " = ? WHERE " + idColumn + " = ?";
+
+        try (Connection con = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+             PreparedStatement pstmt = con.prepareStatement(sql)) {
+            if (updateChoice == 3) {
+                pstmt.setInt(1, Integer.parseInt(newValue));
+            } else {
+                pstmt.setString(1, newValue);
+            }
+            pstmt.setInt(2, id);
+
+            int rowsAffected = pstmt.executeUpdate();
+            if (rowsAffected > 0) {
+                System.out.println("Updated successfully!");
+            } else {
+                System.out.println("Participant not found!");
+            }
+        } catch (SQLException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+    public static void deleteParticipant(Scanner sc){
+        System.out.println("Gender (M/F): ");
+        String gender=sc.nextLine();
+        System.out.println("Participant ID: ");
+        int id=sc.nextInt();
+        sc.nextLine();
+        String table_name=gender.equalsIgnoreCase("F") ? "female_participants":"male_participants";
+        String idColumn=gender.equalsIgnoreCase("F") ? "female_id":"male_id";
+        String sql=" DELETE FROM "+table_name+" WHERE "+idColumn+" = ?";
+        try(Connection con=DriverManager.getConnection(DB_URL,DB_USER,DB_PASSWORD);
+            PreparedStatement pstmt=con.prepareStatement(sql)){
+            pstmt.setInt(1,id);
+            int rowsAffected=pstmt.executeUpdate();
+            String resultWORD=rowsAffected>0 ? "Participant deleted successfully!" : "No such participant";
+            System.out.println(resultWORD);
+        }catch(SQLException e){
+            System.out.println("Error: "+e.getMessage());
+        }
+    }
+    public static void searchParticipantInDB(Scanner sc){
+        System.out.println("Gender (M/F): ");
+        String gender=sc.nextLine();
+        System.out.println("How many participants do you need? (1 or MORE?)");
+        int n=sc.nextInt();
+        sc.nextLine();
+        if(n>1) {
+            System.out.println("Enter the first index: ");
+            int first=sc.nextInt();
+            sc.nextLine();
+            System.out.println("Enter the last index: ");
+            int last=sc.nextInt();
+            sc.nextLine();
+            String table_name=gender.equalsIgnoreCase("F") ? "female_participants":"male_participants";
+            String IdColumn=gender.equalsIgnoreCase("F") ? "female_id":"male_id";
+            String sql="SELECT * FROM "+table_name+" WHERE "+IdColumn+" BETWEEN ? AND ? ORDER BY first_name";
+            try(Connection con=DriverManager.getConnection(DB_URL,DB_USER,DB_PASSWORD);
+                PreparedStatement pstmt=con.prepareStatement(sql)){
+                pstmt.setInt(1,first);
+                pstmt.setInt(2,last);
+                ResultSet rs=pstmt.executeQuery();
+                while(rs.next()) {
+                    System.out.println("\n=== PARTICIPANTS FOUND ===");
+                    System.out.println("ID: " + rs.getInt(IdColumn));
+                    System.out.println("Name: " + rs.getString("first_name") + " " + rs.getString("last_name"));
+                    System.out.println("Age: " + rs.getInt("age"));
+                    System.out.println("T-Shirt: " + rs.getString("t_shirt_size"));
+                    System.out.println("Email: " + rs.getString("email"));
+                }
+            } catch (SQLException e) {
+                System.out.println("Error: " + e.getMessage());
+            }
+        }
+        else {System.out.println("Enter the Participant ID");
+            int id=sc.nextInt();
+            String tableName = gender.equalsIgnoreCase("F") ? "female_participants" : "male_participants";
+            String idColumn = gender.equalsIgnoreCase("F") ? "female_id" : "male_id";
+
+            String sql = "SELECT * FROM " + tableName + " WHERE " + idColumn + " = ? ORDER BY first_name";
+
+            try (Connection con = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+                 PreparedStatement pstmt = con.prepareStatement(sql)) {
+
+                pstmt.setInt(1, id);
+                ResultSet rs = pstmt.executeQuery();
+
+                if (rs.next()) {
+                    System.out.println("\n=== PARTICIPANT FOUND ===");
+                    System.out.println("ID: " + rs.getInt(idColumn));
+                    System.out.println("Name: " + rs.getString("first_name") + " " + rs.getString("last_name"));
+                    System.out.println("Age: " + rs.getInt("age"));
+                    System.out.println("Email: " + rs.getString("email"));
+                    System.out.println("T-Shirt: " + rs.getString("t_shirt_size"));
+                } else {
+                    System.out.println("Participant not found!");
+                }
+            } catch (SQLException e) {
+                System.out.println("Error: " + e.getMessage());
+            }
+        }
+    }
+}
--- src/com/eventmanagement/Main.java
+++ src/com/eventmanagement/Main.java
@@ -9,256 +9,50 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Connection con = null;
-        ResultSet rs = null;
-        Statement stmt = null;
-        try {
-            con = DriverManager.getConnection(DB_URL, "postgres", "200888");
-            stmt = con.createStatement();
-            rs = stmt.executeQuery("select * from female_participants");
-            while (rs.next()) System.out.println(rs.getInt("female_id") + " "
-                    + rs.getString("first_name")
-                    + " " + rs.getString("last_name")
-                    + " " + rs.getInt("age")
-                    + " " + rs.getString("t_shirt_size"));
-        } catch (Exception e) {
-            System.out.println("Exception occured!");
-        } finally {
-            try {
-                con.close();
-            } catch (Exception e) {
-                System.out.println("Exception occured!");
-            }
-        }
-    }
-    private static void addFemaleParticipantToDB(Scanner sc) {
-        System.out.print("First name: ");
-        String firstName = sc.nextLine();
-        System.out.print("Last name: ");
-        String lastName = sc.nextLine();
-        System.out.print("Age: ");
-        int age = sc.nextInt();
-        sc.nextLine();
-        System.out.print("Email: ");
-        String email = sc.nextLine();
-        System.out.print("T-shirt size: ");
-        String tShirtSize = sc.nextLine();
+        while (true) {
+            System.out.println("\nEVENT MANAGEMENT SYSTEM");
+            System.out.println("--- CRUD---");
+            System.out.println("1. Create a female Participant to DB");
+            System.out.println("2. Create a male Participant to DB");
+            System.out.println("3. Read all Female Participants from DB");
+            System.out.println("4. Read all Male Participants from DB");
+            System.out.println("5. Update Participant in DB");
+            System.out.println("6. Delete Participant from DB");
+            System.out.println("7. Search Participant by ID in DB");
+            System.out.println("0. Exit");
+            System.out.print("Choose option: ");
 
-        String sql = "INSERT INTO female_participants (first_name, last_name, age,t_shirt_size,email) VALUES (?, ?, ?, ?, ?)";
-
-        try (Connection con = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
-             PreparedStatement pstmt = con.prepareStatement(sql)) {
-
-            pstmt.setString(1, firstName);
-            pstmt.setString(2, lastName);
-            pstmt.setInt(3, age);
-            pstmt.setString(4, tShirtSize);
-            pstmt.setString(5, email);
-
-            int rowsAffected = pstmt.executeUpdate();
-            if (rowsAffected > 0) {
-                System.out.println("Female participant added to database!");
-            }
-        } catch (SQLException e) {
-            System.out.println("Error: " + e.getMessage());
-        }
-    }
-
-    private static void addMaleParticipantToDB(Scanner sc) {
-        System.out.println("First name: ");
-        String first_name = sc.nextLine();
-        System.out.println("Last name: ");
-        String last_name = sc.nextLine();
-        System.out.println("Age: ");
-        int age = sc.nextInt();
-        sc.nextLine();
-        System.out.println("T-shirt size: ");
-        String T_shirt_size = sc.nextLine();
-        System.out.println("Email: ");
-        String email=sc.nextLine();
-        String sql = "INSERT INTO male_participants (first_name,last_name,age,t_shirt_size,email) VALUES (?,?,?,?,?)";
-
-        try (Connection con = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
-             PreparedStatement pstmt = con.prepareStatement(sql)) {
-            pstmt.setString(1, first_name);
-            pstmt.setString(2, last_name);
-            pstmt.setInt(3, age);
-            pstmt.setString(4, T_shirt_size);
-            pstmt.setString(5, email);
-            int rowsAffected = pstmt.executeUpdate();
-            if (rowsAffected > 0) System.out.println("Female participant added to database!");
-        } catch (SQLException e) {
-            System.out.println("Error: " + e.getMessage());
-        }
-    }
-    private static void viewAllFemaleParticipants(Scanner sc){
-        String sql="SELECT * FROM female_participants";
-        try(Connection con = DriverManager.getConnection(DB_URL,DB_USER,DB_PASSWORD);
-            Statement stmt = con.createStatement();
-            ResultSet rs=stmt.executeQuery(sql)) {
-            System.out.println("Female participants");
-            while(rs.next()){
-                rs.getInt("female_id");
-                rs.getString("first_name");
-                rs.getString("last_name");
-                rs.getInt("age");
-                rs.getString("t_shirt_size");
-                rs.getString("email");
-            }
-        }catch(SQLException e) {
-            System.out.println("Error: " + e.getMessage());
-        }
-    }
-    private static void viewallMaleParticipants(Scanner sc){
-        String sql="SELECT * FROM male_participants";
-        try(Connection con=DriverManager.getConnection(DB_URL,DB_USER,DB_PASSWORD);
-            Statement stmt=con.createStatement();
-            ResultSet rs=stmt.executeQuery(sql)) {
-            System.out.println("Male participants");
-            while(rs.next()){
-                rs.getInt("male_id");
-                rs.getString("first_name");
-                rs.getString("last_name");
-                rs.getInt("age");
-                rs.getString("t_shirt_size");
-                rs.getString("email");
-            }
-        }catch(SQLException e) {
-            System.out.println("Error: "+e.getMessage());
-        }
-    }
-    private static void updateParticipant(Scanner sc){
-        System.out.println("Gender (M/F): ");
-        String gender=sc.nextLine();
-        System.out.println("Participant ID");
-        int id=sc.nextInt();
-        sc.nextLine();
-        System.out.println("\nWhat to update?");
-        System.out.println("1. First Name");
-        System.out.println("2. Last Name");
-        System.out.println("3. Age");
-        System.out.println("4. Email");
-        System.out.println("5. T-Shirt Size");
-        System.out.print("Choose: ");
-        int updateChoice = sc.nextInt();
-        sc.nextLine();
-
-        String tableName = gender.equalsIgnoreCase("F") ? "female_participants" : "male_participants";
-        String idColumn = gender.equalsIgnoreCase("F") ? "female_id" : "male_id";
-        String columnToUpdate = "";
-
-        switch (updateChoice) {
-            case 1: columnToUpdate = "first_name"; break;
-            case 2: columnToUpdate = "last_name"; break;
-            case 3: columnToUpdate = "age"; break;
-            case 4: columnToUpdate = "email"; break;
-            case 5: columnToUpdate = "t_shirt_size"; break;
-            default:
-                System.out.println("Invalid choice!");
-                return;
-        }
-
-        System.out.print("Enter new value: ");
-        String newValue = sc.nextLine();
-
-        String sql = "UPDATE " + tableName + " SET " + columnToUpdate + " = ? WHERE " + idColumn + " = ?";
-
-        try (Connection con = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
-             PreparedStatement pstmt = con.prepareStatement(sql)) {
-            if (updateChoice == 3) {
-                pstmt.setInt(1, Integer.parseInt(newValue));
-            } else {
-                pstmt.setString(1, newValue);
-            }
-            pstmt.setInt(2, id);
-
-            int rowsAffected = pstmt.executeUpdate();
-            if (rowsAffected > 0) {
-                System.out.println("Updated successfully!");
-            } else {
-                System.out.println("Participant not found!");
-            }
-        } catch (SQLException e) {
-            System.out.println("Error: " + e.getMessage());
-        }
-    }
-    private static void deleteParticipant(Scanner sc){
-        System.out.println("Gender (M/F): ");
-        String gender=sc.nextLine();
-        System.out.println("Participant ID: ");
-        int id=sc.nextInt();
-        sc.nextLine();
-        String table_name=gender.equalsIgnoreCase("F") ? "female_participants":"male_participants";
-        String idColumn=gender.equalsIgnoreCase("F") ? "female_id":"male_id";
-        String sql=" DELETE FROM "+table_name+" WHERE "+idColumn+" = ?";
-        try(Connection con=DriverManager.getConnection(DB_URL,DB_USER,DB_PASSWORD);
-            PreparedStatement pstmt=con.prepareStatement(sql)){
-            pstmt.setInt(1,id);
-            int rowsAffected=pstmt.executeUpdate();
-            String resultWORD=rowsAffected>0 ? "Participant deleted successfully!" : "No such participant";
-            System.out.println(resultWORD);
-        }catch(SQLException e){
-            System.out.println("Error: "+e.getMessage());
-        }
-    }
-    private static void searchParticipantInDB(Scanner sc){
-        System.out.println("Gender (M/F): ");
-        String gender=sc.nextLine();
-        System.out.println("How many participants do you need? (1 or MORE?)");
-        int n=sc.nextInt();
-        sc.nextLine();
-        if(n>1) {
-            System.out.println("Enter the first index: ");
-            int first=sc.nextInt();
+            int choice = sc.nextInt();
             sc.nextLine();
-            System.out.println("Enter the last index: ");
-            int last=sc.nextInt();
-            sc.nextLine();
-            String table_name=gender.equalsIgnoreCase("F") ? "female_participants":"male_participants";
-            String IdColumn=gender.equalsIgnoreCase("F") ? "female_id":"male_id";
-            String sql="SELECT * FROM "+table_name+" WHERE "+IdColumn+" BETWEEN ? AND ? ";
-            try(Connection con=DriverManager.getConnection(DB_URL,DB_USER,DB_PASSWORD);
-                PreparedStatement pstmt=con.prepareStatement(sql)){
-                pstmt.setInt(1,first);
-                pstmt.setInt(2,last);
-                ResultSet rs=pstmt.executeQuery();
-                while(rs.next()) {
-                    System.out.println("\n=== PARTICIPANTS FOUND ===");
-                    System.out.println("ID: " + rs.getInt(IdColumn));
-                    System.out.println("Name: " + rs.getString("first_name") + " " + rs.getString("last_name"));
-                    System.out.println("Age: " + rs.getInt("age"));
-                    System.out.println("T-Shirt: " + rs.getString("t_shirt_size"));
-                    System.out.println("Email: " + rs.getString("email"));
-                }
-            } catch (SQLException e) {
-                System.out.println("Error: " + e.getMessage());
-            }
-        }
-        else {System.out.println("Enter the Participant ID");
-            int id=sc.nextInt();
-            String tableName = gender.equalsIgnoreCase("F") ? "female_participants" : "male_participants";
-            String idColumn = gender.equalsIgnoreCase("F") ? "female_id" : "male_id";
 
-            String sql = "SELECT * FROM " + tableName + " WHERE " + idColumn + " = ?";
-
-            try (Connection con = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
-                 PreparedStatement pstmt = con.prepareStatement(sql)) {
-
-                pstmt.setInt(1, id);
-                ResultSet rs = pstmt.executeQuery();
-
-                if (rs.next()) {
-                    System.out.println("\n=== PARTICIPANT FOUND ===");
-                    System.out.println("ID: " + rs.getInt(idColumn));
-                    System.out.println("Name: " + rs.getString("first_name") + " " + rs.getString("last_name"));
-                    System.out.println("Age: " + rs.getInt("age"));
-                    System.out.println("Email: " + rs.getString("email"));
-                    System.out.println("T-Shirt: " + rs.getString("t_shirt_size"));
-                } else {
-                    System.out.println("Participant not found!");
-                }
-            } catch (SQLException e) {
-                System.out.println("Error: " + e.getMessage());
+            switch (choice) {
+                case 1:
+                    DatabaseOperations.addFemaleParticipantToDB(sc);
+                    break;
+                case 2:
+                    DatabaseOperations.addFemaleParticipantToDB(sc);
+                    break;
+                case 3:
+                    DatabaseOperations.viewAllFemaleParticipants();
+                    break;
+                case 4:
+                    DatabaseOperations.viewAllMaleParticipants();
+                    break;
+                case 5:
+                    DatabaseOperations.updateParticipant(sc);
+                    break;
+                case 6:
+                    DatabaseOperations.deleteParticipant(sc);
+                    break;
+                case 7:
+                    DatabaseOperations.searchParticipantInDB(sc);
+                    break;
+                case 0:
+                    System.out.println("Exiting...");
+                    sc.close();
+                    return;
+                default:
+                    System.out.println("Invalid option!");
             }
         }
     }
